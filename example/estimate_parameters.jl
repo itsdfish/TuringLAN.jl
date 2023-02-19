3b38384db7d5e7260682c349caa157ce7aa32978
itsdfish <itsdfish@gmail.com>
2023-02-19 16:20:41 -0500

    fix batch logpdf

--- example/estimate_parameters.jl
+++ example/estimate_parameters.jl
@@ -28,25 +28,25 @@ end
 
 loglikelihood(d::VSModel, data::Vector{<:Real}) = logpdf(d, data)
 
-function logpdf(m::VSModel, data::Vector{<:Real})
-    μ = m.μ
-    model = m.neural_net
-    σ = 1.0
-    LLs = map(x -> model([μ,σ,x])[1], data)
-    return sum(LLs)
-end
-
 # function logpdf(m::VSModel, data::Vector{<:Real})
-#     n = length(data)
 #     μ = m.μ
 #     model = m.neural_net
 #     σ = 1.0
-#     m_data = Float32[fill(μ, 1, n),
-#                     fill(σ, 1, n),
-#                     x']
-#     LLs = model(m_data)
+#     LLs = map(x -> model([μ,σ,x])[1], data)
 #     return sum(LLs)
 # end
+
+function logpdf(m::VSModel, data::Vector{<:Real})
+    n = length(data)
+    μ = m.μ
+    model = m.neural_net
+    σ = 1.0
+    m_data = Float32[fill(μ, 1, n);
+                    fill(σ, 1, n);
+                    data']
+    LLs = model(m_data)
+    return sum(LLs)
+end
 
 @model function my_model(model, data)
     μ ~ Uniform(-3, 3)
@@ -57,7 +57,7 @@ data = Float32.(rand(Normal(0, 1), 150))
 
 # Turing.setadbackend(:forwarddiff)
 # Turing.setadbackend(:reversediff)
-# Turing.setadbackend(:zygote)
+Turing.setadbackend(:zygote)
 
 delta = 0.65
 n_adapt = 1000
